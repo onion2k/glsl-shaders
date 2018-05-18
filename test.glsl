7be3ce2cf48da278816a53ad162f87ca5e955a40
Chris Neale <chris@komododigital.co.uk>
2018-05-18 09:48:05 +0100

    iq.glsl working

--- test.glsl
+++ test.glsl
@@ -21,176 +21,18 @@ const float EPSILON = 0.0001;
  */
 //------------------------------------------------------------------
 
-float sdPlane( vec3 p )
-{
-	return p.y;
-}
-
 float sdSphere( vec3 p, float s )
 {
     return length(p)-s;
 }
 
-float sdBox( vec3 p, vec3 b )
-{
-    vec3 d = abs(p) - b;
-    return min(max(d.x,max(d.y,d.z)),0.0) + length(max(d,0.0));
-}
-
-float sdEllipsoid( in vec3 p, in vec3 r )
-{
-    return (length( p/r ) - 1.0) * min(min(r.x,r.y),r.z);
-}
-
-float udRoundBox( vec3 p, vec3 b, float r )
-{
-    return length(max(abs(p)-b,0.0))-r;
-}
-
-float sdTorus( vec3 p, vec2 t )
-{
-    return length( vec2(length(p.xz)-t.x,p.y) )-t.y;
-}
-
-float sdHexPrism( vec3 p, vec2 h )
-{
-    vec3 q = abs(p);
-#if 0
-    return max(q.z-h.y,max((q.x*0.866025+q.y*0.5),q.y)-h.x);
-#else
-    float d1 = q.z-h.y;
-    float d2 = max((q.x*0.866025+q.y*0.5),q.y)-h.x;
-    return length(max(vec2(d1,d2),0.0)) + min(max(d1,d2), 0.);
-#endif
-}
-
-float sdCapsule( vec3 p, vec3 a, vec3 b, float r )
-{
-	vec3 pa = p-a, ba = b-a;
-	float h = clamp( dot(pa,ba)/dot(ba,ba), 0.0, 1.0 );
-	return length( pa - ba*h ) - r;
-}
-
-float sdEquilateralTriangle(  in vec2 p )
-{
-    const float k = 1.73205;//sqrt(3.0);
-    p.x = abs(p.x) - 1.0;
-    p.y = p.y + 1.0/k;
-    if( p.x + k*p.y > 0.0 ) p = vec2( p.x - k*p.y, -k*p.x - p.y )/2.0;
-    p.x += 2.0 - 2.0*clamp( (p.x+2.0)/2.0, 0.0, 1.0 );
-    return -length(p)*sign(p.y);
-}
-
-float sdTriPrism( vec3 p, vec2 h )
-{
-    vec3 q = abs(p);
-    float d1 = q.z-h.y;
-#if 1
-    // distance bound
-    float d2 = max(q.x*0.866025+p.y*0.5,-p.y)-h.x*0.5;
-#else
-    // correct distance
-    h.x *= 0.866025;
-    float d2 = sdEquilateralTriangle(p.xy/h.x)*h.x;
-#endif
-    return length(max(vec2(d1,d2),0.0)) + min(max(d1,d2), 0.);
-}
-
-float sdCylinder( vec3 p, vec2 h )
-{
-  vec2 d = abs(vec2(length(p.xz),p.y)) - h;
-  return min(max(d.x,d.y),0.0) + length(max(d,0.0));
-}
-
-float sdCone( in vec3 p, in vec3 c )
-{
-    vec2 q = vec2( length(p.xz), p.y );
-    float d1 = -q.y-c.z;
-    float d2 = max( dot(q,c.xy), q.y);
-    return length(max(vec2(d1,d2),0.0)) + min(max(d1,d2), 0.);
-}
-
-float sdConeSection( in vec3 p, in float h, in float r1, in float r2 )
-{
-    float d1 = -p.y - h;
-    float q = p.y - h;
-    float si = 0.5*(r1-r2)/h;
-    float d2 = max( sqrt( dot(p.xz,p.xz)*(1.0-si*si)) + q*si - r2, q );
-    return length(max(vec2(d1,d2),0.0)) + min(max(d1,d2), 0.);
-}
-
-float sdPryamid4(vec3 p, vec3 h ) // h = { cos a, sin a, height }
-{
-    // Tetrahedron = Octahedron - Cube
-    float box = sdBox( p - vec3(0,-2.0*h.z,0), vec3(2.0*h.z) );
- 
-    float d = 0.0;
-    d = max( d, abs( dot(p, vec3( -h.x, h.y, 0 )) ));
-    d = max( d, abs( dot(p, vec3(  h.x, h.y, 0 )) ));
-    d = max( d, abs( dot(p, vec3(  0, h.y, h.x )) ));
-    d = max( d, abs( dot(p, vec3(  0, h.y,-h.x )) ));
-    float octa = d - h.z;
-    return max(-box,octa); // Subtraction
- }
-
-float length2( vec2 p )
-{
-	return sqrt( p.x*p.x + p.y*p.y );
-}
-
-float length6( vec2 p )
-{
-	p = p*p*p; p = p*p;
-	return pow( p.x + p.y, 1.0/6.0 );
-}
-
-float length8( vec2 p )
-{
-	p = p*p; p = p*p; p = p*p;
-	return pow( p.x + p.y, 1.0/8.0 );
-}
-
-float sdTorus82( vec3 p, vec2 t )
-{
-    vec2 q = vec2(length2(p.xz)-t.x,p.y);
-    return length8(q)-t.y;
-}
-
-float sdTorus88( vec3 p, vec2 t )
-{
-    vec2 q = vec2(length8(p.xz)-t.x,p.y);
-    return length8(q)-t.y;
-}
-
-float sdCylinder6( vec3 p, vec2 h )
-{
-    return max( length6(p.xz)-h.x, abs(p.y)-h.y );
-}
-
 //------------------------------------------------------------------
-
-float opS( float d1, float d2 )
-{
-    return max(-d2,d1);
-}
-
-vec2 opU( vec2 d1, vec2 d2 )
-{
-	return (d1.x<d2.x) ? d1 : d2;
-}
 
 vec3 opRep( vec3 p, vec3 c )
 {
     return mod(p,c)-0.5*c;
 }
 
-vec3 opTwist( vec3 p )
-{
-    float  c = cos(10.0*p.y+10.0);
-    float  s = sin(10.0*p.y+10.0);
-    mat2   m = mat2(c,-s,s,c);
-    return vec3(m*p.xz,p.y);
-}
 /**
  * Signed distance function describing the scene.
  * 
@@ -198,38 +40,14 @@ vec3 opTwist( vec3 p )
  * Sign indicates whether the point is inside or outside the surface,
  * negative indicating inside.
  */
+
 float sceneSDF(vec3 samplePoint) {
   
   vec3 pos = vec3(cos(u_time)*2.6,sin(u_time)*1.6,sin(u_time)*1.6);
-  
   return sdSphere(opRep( pos + samplePoint, vec3(5.5,5.,5.)), 0.5);
 
-//   vec2 res = vec2(0.,0.);
-//   vec3 pos = vec3(0.,0.,0.);
-//   return opU( 
-//     res, 
-//     vec2( 
-//         opS( 
-//             sdTorus82( 
-//                 pos-vec3(-2.0,0.2, 0.0), 
-//                 vec2(0.20,0.1)
-//             ), 
-//             sdTorus88( 
-//                 opRep( 
-//                     vec3(
-//                         atan(pos.x+2.0,pos.z)/6.2831, 
-//                         pos.y, 
-//                         0.02+0.5*length(pos-vec3(-2.0,0.2, 0.0))
-//                     ), 
-//                     vec3(0.05, 1.0, 0.05)
-//                 ), 
-//                 vec2(0.02, 0.6)
-//             )
-//         ),
-//         51.0
-//     )
-// );
 }
+
 
 /**
  * Return the shortest distance from the eyepoint to the scene surface along
@@ -358,6 +176,8 @@ vec3 phongIllumination(vec3 k_a, vec3 k_d, vec3 k_s, float alpha, vec3 p, vec3 e
   return color;
 }
 
+
+
 void main()
 {
 	vec3 dir = rayDirection(45.0, u_resolution.xy, gl_FragCoord.xy);
@@ -373,9 +193,9 @@ void main()
     // The closest point on the surface to the eyepoint along the view ray
     vec3 p = eye + dist * dir;
     
-    vec3 K_a = vec3(0.2, 0.2, 0.2);
+    vec3 K_a = vec3(0.2, 0.2, 0.2); // ambient?
     vec3 K_d = vec3(0.2, 0.2, 0.5); // color
-    vec3 K_s = vec3(1.0, 1.0, 1.0);
+    vec3 K_s = vec3(1.0, 1.0, 1.0); // shininess ?
     float shininess = 20.0;
     
     vec3 color = phongIllumination(K_a, K_d, K_s, shininess, p, eye);
