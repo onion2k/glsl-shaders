cb3418971849956e3f9211af2803a52f12507c46
Chris Neale <chris@komododigital.co.uk>
2018-05-31 09:23:18 +0100

    annotating smin

--- libs/smin-blend.glsl
+++ libs/smin-blend.glsl
@@ -1,6 +1,9 @@
+// a is position 1
+// b is position 2
+// k is bias?
 
 float sdf_smin(float a, float b, float k)
 {
-	float res = exp(-k*a) + exp(-k*b);
-	return -log(max(0.0001,res)) / k;
+	float res = exp(-k * a) + exp(-k * b);
+	return -log(max(0.0001, res)) / k;
 }
