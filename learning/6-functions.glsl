6d91488c68113932800c2355e7d38d7cc12e1186
Chris Neale <chris@komododigital.co.uk>
2018-06-04 14:42:40 +0100

    Fixed noise

--- learning/6-functions.glsl
+++ learning/6-functions.glsl
@@ -1,28 +1,25 @@
   precision mediump float;
 
-  uniform vec2      u_resolution;           // viewport resolution (in pixels)
-  uniform float     u_time;                 // shader playback time (in seconds)
+  uniform vec2      u_resolution;
+  uniform float     u_time;
 
   float random(vec2 c){
     return fract(sin(dot(c.xy ,vec2(12.9898,78.233))) * 43758.5453);
   }
-
-  const float interval = 3.0;
   
   void main() {
 
-    vec3 color = vec3(1.0,0.4,0.4);
+    vec3 color = vec3(1.0);
 
     // Random number.
     vec2 pos = gl_FragCoord.xy;
-    pos *= sin(u_time);
+         pos *= sin(u_time);
     float r = random(pos);
 
     // Noise color using random number.
     vec3 noise = vec3(r);
-    float noise_intensity = 0.25;
+    float noise_intensity = 0.5;
 
-    // Combined colors.
     color = mix(color, noise, noise_intensity);
 
     gl_FragColor = vec4(color, 1.0);
