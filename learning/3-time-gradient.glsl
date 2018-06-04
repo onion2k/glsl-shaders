490673bed6859d8e2444f80df6d6a2b32deda21f
Chris Neale <chris@komododigital.co.uk>
2018-06-04 08:06:09 +0100

    Lesson 3 - timed gradients

--- learning/3-time-gradient.glsl
+++ learning/3-time-gradient.glsl
@@ -1,5 +1,7 @@
+
 precision mediump float;
 
+uniform float u_time;
 uniform vec2 u_resolution;
 
 void main()
@@ -7,6 +9,9 @@ void main()
     // Normalized pixel coordinates (from 0 to 1)
     vec2 uv = gl_FragCoord.xy / u_resolution.xy;
 
+    // Time varying pixel color
+    vec3 col = 0.5 + 0.5 * cos(u_time + uv.xyx + vec3(0,2,4));
+
     // Output to screen
-    gl_FragColor = vec4(1.0, 1.0, uv.y, 1.0);
+    gl_FragColor = vec4(col,1.0);
 }
