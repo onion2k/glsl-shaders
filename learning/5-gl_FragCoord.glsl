6f3c66103cdb1eada4837543aff0c459f39004ad
Chris Neale <chris@komododigital.co.uk>
2018-06-06 08:34:56 +0100

    Tidied up whitespace

--- learning/5-gl_FragCoord.glsl
+++ learning/5-gl_FragCoord.glsl
@@ -1,24 +1,20 @@
-  precision mediump float;
+precision mediump float;
 
-  uniform vec2 u_resolution;
-  uniform float u_time;
+uniform vec2 u_resolution;
+uniform float u_time;
 
-  mat2 rot2(float r){
-    float c=cos(r), s=sin(r);
-    return mat2(c, s, -s, c);
-  }
+mat2 rot2(float r){
+  float c=cos(r), s=sin(r);
+  return mat2(c, s, -s, c);
+}
 
-  void main(void)
-  {
-      vec2 uv = gl_FragCoord.xy / u_resolution.y;
+void main(void)
+{
+    vec2 uv = gl_FragCoord.xy / u_resolution.y;
 
-      vec2 uvr = rot2(u_time) * (0.25 - uv);
+    vec2 uvr = rot2(u_time) * (0.25 - uv);
 
-      float cb = floor(uvr.x * 10.0) + floor(uvr.y * 10.0);
+    float cb = floor(uvr.x * 10.0) + floor(uvr.y * 10.0);
 
-      gl_FragColor = vec4(1.,1.,1.,mod(cb, 2.0));
-  }
-
-
-
-// 
+    gl_FragColor = vec4(1.,1.,1.,mod(cb, 2.0));
+}
