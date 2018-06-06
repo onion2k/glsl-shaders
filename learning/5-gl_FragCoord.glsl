a1643460c635facae8f54cdf00903c71fc8a158b
Chris Neale <chris@komododigital.co.uk>
2018-06-06 08:34:02 +0100

    Rotation

--- learning/5-gl_FragCoord.glsl
+++ learning/5-gl_FragCoord.glsl
@@ -3,9 +3,22 @@
   uniform vec2 u_resolution;
   uniform float u_time;
 
+  mat2 rot2(float r){
+    float c=cos(r), s=sin(r);
+    return mat2(c, s, -s, c);
+  }
+
   void main(void)
   {
-      vec2 uv = gl_FragCoord.xy / u_resolution.x;
-      float cb = floor((0.5 - uv.x + cos(gl_FragCoord.x / u_resolution.x)) * 30.) + floor(( uv.y + sin(gl_FragCoord.x / u_resolution.x)) * 30.);
+      vec2 uv = gl_FragCoord.xy / u_resolution.y;
+
+      vec2 uvr = rot2(u_time) * (0.25 - uv);
+
+      float cb = floor(uvr.x * 10.0) + floor(uvr.y * 10.0);
+
       gl_FragColor = vec4(1.,1.,1.,mod(cb, 2.0));
   }
+
+
+
+// 
