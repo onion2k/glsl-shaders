142caeb4bb050e354eaaf624fc289279af817709
Chris Neale <chris@komododigital.co.uk>
2018-06-07 08:34:32 +0100

    Annotated rotations

--- learning/5b-matrixtranslate.glsl
+++ learning/5b-matrixtranslate.glsl
@@ -11,12 +11,17 @@ mat2 rot2(float r){
 
 void main(void)
 {
+    // Normalized position again.
     vec2 uv = gl_FragCoord.xy / u_resolution.x;
-    float aspect = u_resolution.y / u_resolution.x;
 
-    vec2 uvr = uv * rot2(u_time * 0.1);
+    // Subtract a vec2 from the position in order to move the center of rotation. In this
+    // case we're using the center of the canvas, but that won't work correctly if your
+    // canvas isn't square. Fortunately ours is.
+    vec2 uvr = (uv - vec2(0.5, 0.5));
 
-    uvr = (uv - vec2(0.5, 0.5 * aspect)) * rot2(u_time * 0.1);
+    // Take the new coordinates and multiply them by a mat2 (2d matrix) in order to rotate
+    // them. This time our rotation angle is based on the time, so we get a rotating effect.
+    uvr = uvr * rot2(u_time * 0.1);
 
     // The same checkerboard as before, but this time using our new rotated and translated vector.
     float cb = floor(uvr.x * 10.0) + floor(uvr.y * 10.0);
