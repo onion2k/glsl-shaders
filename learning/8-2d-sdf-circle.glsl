918ae1507005cafa8ea7b0718aa044ceb20f053c
Chris Neale <chris@komododigital.co.uk>
2018-06-11 16:05:21 +0100

    Moved uv

--- learning/8-2d-sdf-circle.glsl
+++ learning/8-2d-sdf-circle.glsl
@@ -1,40 +1,41 @@
 
-  #extension GL_OES_standard_derivatives : enable
+#extension GL_OES_standard_derivatives : enable
 
-  #ifdef GL_ES
-  precision mediump float;
-  #endif
+#ifdef GL_ES
+precision mediump float;
+#endif
 
-  uniform float u_time;
-  uniform vec2 u_resolution;
+uniform float u_time;
+uniform vec2 u_resolution;
 
-  float aastep(float threshold, float value) {
-    #ifdef GL_OES_standard_derivatives
-      float afwidth = length(vec2(dFdx(value), dFdy(value))) * 0.70710678118654757;
-      return smoothstep(threshold-afwidth, threshold+afwidth, value);
-    #else
-      return step(threshold, value);
-    #endif  
-  }
+float aastep(float threshold, float value) {
+  #ifdef GL_OES_standard_derivatives
+    float afwidth = length(vec2(dFdx(value), dFdy(value))) * 0.70710678118654757;
+    return smoothstep(threshold-afwidth, threshold+afwidth, value);
+  #else
+    return step(threshold, value);
+  #endif  
+}
 
-  float circle(in vec2 st, float radius) {
-    return length(st) - radius;
-  }
+float circle(in vec2 st, float radius) {
+  return length(st) - radius;
+}
 
-  vec2 repeat(vec2 dist, vec2 size) {
-    return mod(dist,size) - 0.5 * size;
-  }
+vec2 repeat(vec2 dist, vec2 size) {
+  return mod(dist,size) - 0.5 * size;
+}
 
-  void main(void)
-  {
-      vec2 uv = gl_FragCoord.xy / u_resolution;
-          uv.x *= u_resolution.x / u_resolution.y;
+void main(void)
+{
+  vec2 uv = gl_FragCoord.xy / u_resolution;
+      uv.x *= u_resolution.x / u_resolution.y;
 
-      uv = repeat(uv, vec2(0.2, 0.25));
-      float c = circle(uv, 0.05);
+  uv -= vec2(0.5,0.5);
+  uv  = repeat(uv, vec2(0.25, 0.25));
+  float c = circle(uv, 0.1);
 
-      c = aastep(0.025, c);
+  c = aastep(0.025, c);
 
-      vec3 color = mix(vec3(0.0,0.0,0.0), vec3(1.0,1.0,1.0), c);
-      gl_FragColor = vec4(color, 1.0);
-  }
+  vec3 color = mix(vec3(0.0,0.0,0.0), vec3(1.0,1.0,1.0), c);
+  gl_FragColor = vec4(color, 1.0);
+}
