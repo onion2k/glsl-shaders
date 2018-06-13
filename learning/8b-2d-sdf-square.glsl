13d018b9b588eafe3b45aa86d9f5d5f6b6b70be4
Chris Neale <chris@komododigital.co.uk>
2018-06-13 08:50:55 +0100

    SDF Squares

--- learning/8b-2d-sdf-square.glsl
+++ learning/8b-2d-sdf-square.glsl
@@ -1,3 +1,4 @@
+#extension GL_OES_standard_derivatives : enable
 
 #ifdef GL_ES
 precision mediump float;
@@ -13,6 +14,11 @@ float aastep(float threshold, float value) {
   #else
     return step(threshold, value);
   #endif  
+}
+
+mat2 rot2(float r){
+  float c=cos(r), s=sin(r);
+  return mat2(c, s, -s, c);
 }
 
 float square(in vec2 st, vec2 dm) {
@@ -38,17 +44,19 @@ void main(void)
 {
   vec2 uv = gl_FragCoord.xy / min(u_resolution.x, u_resolution.y);
 
+  uv *= rot2(0.785);
   uv  = repeat(uv, vec2(0.25, 0.25));
-  float c = circle(uv, 0.05);
+  float c = square(uv, vec2(0.0005));
 
-//   uv = gl_FragCoord.xy / min(u_resolution.x, u_resolution.y);
-//   uv -= vec2(0.5,0.5);
-//   uv  = repeat(uv - vec2(0.125, 0.125), vec2(0.25, 0.25));
-//   float c2 = circle(uv, clamp(sin(u_time) * 0.5, 0.025, 0.25));
+  uv = gl_FragCoord.xy / min(u_resolution.x, u_resolution.y);
+  uv -= vec2(0.25,0.25);
+  uv  = repeat(uv, vec2(0.25, 0.25));
+  float l = clamp(sin(u_time) * 0.5, 0.0025, 0.25);
+  float c2 = square(uv, vec2(l));
 
-//   float cc = min(c,c2);
+   float cc = max(c,c2);
 
-  c = aastep(0.025, c);
+  c = aastep(0.025, cc);
 
   vec3 color = mix(vec3(0.0,0.0,0.0), vec3(1.0,1.0,1.0), c);
   gl_FragColor = vec4(color, 1.0);
