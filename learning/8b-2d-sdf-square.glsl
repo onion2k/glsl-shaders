38cdfc1d23ba6683632cd23f38bb62c48ebc5021
Chris Neale <chris@komododigital.co.uk>
2018-06-15 08:35:51 +0100

    Rocking squares

--- learning/8b-2d-sdf-square.glsl
+++ learning/8b-2d-sdf-square.glsl
@@ -33,7 +33,7 @@ float square(in vec2 st, vec2 dm) {
 }
 
 float circle(in vec2 st, float radius) {
-  return length(st - vec2(0.5)) - radius;
+  return length(st - vec2(0.5)) - radius * 0.5;
 }
 
 vec2 repeat(vec2 dist, vec2 size) {
@@ -44,21 +44,25 @@ void main(void)
 {
   vec2 uv = gl_FragCoord.xy / min(u_resolution.x, u_resolution.y);
 
-  // uv *= rot2(0.785);
+  uv -= vec2(0.5,0.5);
+  uv *= rot2(cos(u_time) * 0.1);
   uv  = repeat(uv, vec2(4.0, 4.0));
   float x = 0.7;
   float y = 0.7; // + (sin(u_time) * 0.2);
-  float c = square(uv, vec2(x, y));
+  float c1 = square(uv, vec2(x, y));
 
-  uv = gl_FragCoord.xy / min(u_resolution.x, u_resolution.y);
-  // uv -= vec2(smoothstep(fract(u_time * 0.1), 0.0, 1.0), smoothstep(fract(u_time * 0.1), 0.0, 1.0));
-  uv  = repeat(uv, vec2(4.0, 4.0));
-  // float l = clamp(sin(u_time) * 2.0, 0.2, 1.0);
-  float c2 = circle(uv, 0.25);
+  // uv = gl_FragCoord.xy / min(u_resolution.x, u_resolution.y);
+  // uv -= vec2(u_time * 0.1, u_time * 0.1);
+  // uv *= rot2(cos(u_time) * 0.001);
+  // uv  = repeat(uv, vec2(4.0, 4.0));
+  // // float l = clamp(sin(u_time) * 2.0, 0.2, 1.0);
+  // float c2 = circle(uv, 0.1);
+  // c2 = smoothstep(c2, 0.1, 0.15);
+  // c2 = step(0.1, c2);
 
-  float cc = min(c, c2);
+  float c = c1; //max(c1, c2);
 
-  c = aastep(0.01, cc);
+  // c = aastep(0.01, c);
 
   vec3 color = mix(vec3(0.0,0.0,0.0), vec3(1.0,1.0,1.0), c);
   gl_FragColor = vec4(color, 1.0);
