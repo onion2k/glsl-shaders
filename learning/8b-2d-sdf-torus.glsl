cabf3819fbc3232a1e1a5858de93cd5d284fdbb8
Chris Neale <chris@komododigital.co.uk>
2018-06-17 17:29:59 +0100

    Update 8b-2d-sdf-torus.glsl

--- learning/8b-2d-sdf-torus.glsl
+++ learning/8b-2d-sdf-torus.glsl
@@ -1,8 +1,6 @@
 #extension GL_OES_standard_derivatives : enable
 
-#ifdef GL_ES
 precision mediump float;
-#endif
 
 uniform float u_time;
 uniform vec2 u_resolution;
@@ -16,20 +14,10 @@ float aastep(float threshold, float value) {
   #endif  
 }
 
+// Here we define a rotation matrix. It's a basic 2,2 matrix based on trig.
 mat2 rot2(float r){
   float c=cos(r), s=sin(r);
   return mat2(c, s, -s, c);
-}
-
-float square(in vec2 st, vec2 dm) {
-
-    vec2 bl = step(0.5 - dm / 2.0, st);
-    float pct = bl.x * bl.y;
-
-    vec2 tr = step(st, 0.5 + dm / 2.0);
-    pct *= tr.x * tr.y;
-
-    return pct;
 }
 
 float circle(in vec2 st, float radius) {
@@ -45,9 +33,16 @@ void main(void)
   vec2 uv = gl_FragCoord.xy / min(u_resolution.x, u_resolution.y);
 
   uv = gl_FragCoord.xy / min(u_resolution.x, u_resolution.y);
+
+  // Move the coordinate system to move the SDF geometry
   uv -= vec2(u_time * 0.1, u_time * 0.1);
+  
+  // Rotate the entire coordinate system
   uv *= rot2(cos(u_time) * 0.001);
+  
+  // Our mod based repeating function
   uv  = repeat(uv, vec2(4.0, 4.0));
+  
   
   float c2 = circle(uv, 0.1);
   c2 = smoothstep(c2, 0.1, 0.15);
